97a64049b73ec08051b5137e59a574f370e12e2f
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-03-01 12:04:52 +0100

    Create first page

---
+++ index.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+
+	$("#dropbtnBook1").click(function(){
+        $("#Book1").slideToggle("slow");
+    });
+    $("#dropbtnBook2").click(function(){
+        $("#Book2").slideToggle("slow");
+    });
+	$("#dropbtnBook3").click(function(){
+        $("#Book3").slideToggle("slow");
+    });
+
+});
