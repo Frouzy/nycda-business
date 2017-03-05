27bde6efa1c6c2cc9348cf17786411a75683f803
obendjaballah <omarbendjaballah@hotmail.com>
2017-03-05 22:05:16 +0100

    2nd commit: create OrderNow.js file and added JQuery annimation to the OrderNow.html page.

---
+++ animation/orderNow.js
@@ -0,0 +1,12 @@
+$(document).ready(
+					//Code that executes when the document is ready
+					function(){
+					    alert('The document is ready, sah!');
+					  }
+					);
+
+$("body").click( function(){
+    $("body").fadeOut (4000);
+    $("body").fadeIn (4000);
+    $("body").hide(4000);
+    $("body").show(4000);
